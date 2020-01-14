b0502bf0a64c014906fc1d4dedc7f31d48f67fa0
yshr10ic <yshr10ic@gmail.com>
2020-01-14 20:52:59 +0900

    create dotnet solution

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace deep_learning_from_scratch_csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
